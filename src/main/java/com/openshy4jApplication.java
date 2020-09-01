4cd625b6c054164744ea60466936a46ba306fc8d
SIEUN <shyumee@gmail.com>
2020-09-01 12:28:53 +0900

    Initial commit

---
+++ src/main/java/com/openshy4jApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class openshy4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(openshy4jApplication.class, args);
+    }
+}
