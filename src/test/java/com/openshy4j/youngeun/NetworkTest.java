3afeb4aa61585bfa9f4671d9256561840f94c7da
lucia <lucia.son.dev@gmail.com>
2020-09-15 13:11:53 +0900

    1branch

--- src/test/java/com/openshy4j/youngeun/NetworkTest.java
+++ src/test/java/com/openshy4j/youngeun/NetworkTest.java
@@ -8,10 +8,7 @@ public class NetworkTest {
 
   @Test
   public void 테스트() {
-    System.out.println("one more test");
-    System.out.println("branch made");
-    System.out.println("branch made");
-    System.out.println("pull request");
+    System.out.println("gitgitgit");
   }
 
 }
