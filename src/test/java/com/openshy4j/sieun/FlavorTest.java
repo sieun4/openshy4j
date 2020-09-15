59dbdd873d532137b75214fb82f4442260f8284b
SIEUN <shyumee@gmail.com>
2020-09-15 11:27:29 +0900

    .

--- src/test/java/com/openshy4j/sieun/FlavorTest.java
+++ src/test/java/com/openshy4j/sieun/FlavorTest.java
@@ -1,2 +1,17 @@
-package com.openshy4j.sieun;public class Test {
+package com.openshy4j.sieun;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FlavorTest {
+
+  @Test
+  public void 테스트(){
+
+    System.out.println("hello :D");
+
+  }
 }
+
+
