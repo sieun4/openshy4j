d692ab7735d0d8972e9ada77aa1670534627984a
HanGuk <hanguk0726@gmail.com>
2020-09-15 10:14:33 +0900

    setting

--- src/main/java/com/openshy4j/web/IndexController.java
+++ src/main/java/com/openshy4j/web/IndexController.java
@@ -1,38 +1,23 @@
 package com.openshy4j.web;
 
 
-import com.openshy4j.service.posts.PostsService;
-import com.openshy4j.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        return "index";
+    public RedirectView index() {
+        return new RedirectView("index");
     }
 
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
-    }
 
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model
-                                                    model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
-    }
 
 }
---
+++ src/test/java/com/openshy4j/hanguk/ImageTest.java
@@ -0,0 +1,17 @@
+package com.openshy4j.hanguk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImageTest {
+
+  @Test
+  public void 테스트(){
+    System.out.println("test");
+  }
+
+}
