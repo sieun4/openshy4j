2d3d5e80376620bc0e78bd98abdd32e38be8b8be
gmldbd94 <gmldbd94@gmail.com>
2020-09-15 10:30:15 +0900

    pratice branch

--- src/test/java/com/openshy4j/huiyung/Test_openstack.java
+++ src/test/java/com/openshy4j/huiyung/Test_openstack.java
@@ -1,4 +1,12 @@
 package com.openshy4j.huiyung;
 
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class Test_openstack {
+    @Test
+    public void 테스트(){
+        System.out.println("hello world");
+    }
 }
