7ad789be8f1d6603c42b1c606dd674ecb93d88d0
gmldbd94 <gmldbd94@gmail.com>
2020-10-27 12:01:51 +0900

    testing

--- src/test/java/com/openshy4j/TESTopenshy4j.java
+++ src/test/java/com/openshy4j/TESTopenshy4j.java
@@ -1,28 +1,28 @@
 
-package com.openshy4j;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class TESTopenshy4j {
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void getImages() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/images").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
-    }
-}
+//package com.openshy4j;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class TESTopenshy4j {
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+////    @Test
+////    public void getVolume() throws Exception{
+////        mockMvc.perform(MockMvcRequestBuilders.get("/volumes").accept(MediaType.ALL)).andExpect(status().isOk()).andReturn();
+////    }
+//}
